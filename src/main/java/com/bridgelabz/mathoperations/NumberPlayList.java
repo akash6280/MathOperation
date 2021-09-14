a22d76ff6c63c4fbe8c191985f18efa9bc74c407
Akash Singh <akashsingh6280@gmail.com>
2021-09-14 22:44:03 +0530

    ADDED: demonstration of forEach for different classes

---
+++ src/main/java/com/bridgelabz/mathoperations/NumberPlayList.java
@@ -0,0 +1,44 @@
+package com.bridgelabz.mathoperations;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.function.Consumer;
+
+
+public class NumberPlayList {
+	public static void main(String[] args) {
+		List<Integer> myList = new ArrayList<Integer>();
+		for(int i=0;i<5;i++) {
+			myList.add(i);
+		}
+		Iterator<Integer> itr = myList.iterator();
+		while(itr.hasNext()) {
+			System.out.println(itr.next());
+		}
+		
+	class MyConsumer implements Consumer<Integer>{
+		public void accept(Integer number) {
+			System.out.println("Consumer implementation value: "+number);
+		}
+	}
+	MyConsumer action = new MyConsumer();
+	myList.forEach(action);
+	
+	myList.forEach(new Consumer<Integer>() {
+		public void accept(Integer number) {
+			System.out.println("Anonymous class value: "+number);
+		}
+	});
+
+	Consumer<Integer> listAction = n->{
+		System.out.println("Explicit lambda implementation value:"+n);
+	};
+	myList.forEach(listAction);
+	
+	myList.forEach(n->{
+		System.out.println("Implicit lambda implementation value: "+n);
+	});
+	
+	}
+}
